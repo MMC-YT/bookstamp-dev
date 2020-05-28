7f079da0a0ffed765c1f3c19b197e09131005237
ChenLiZhan <ChenLiZhan@users.noreply.github.com>
2020-05-28 15:37:09 +0000

    Deploy Commit

---
+++ precache-manifest.94bda56c49d7aa0b7d0d72bc56706393.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a0fd4229da1b340bace",
+    "url": "/css/chunk-08c37ec1.8c444979.css"
+  },
+  {
+    "revision": "bf2c307e6401511b9582",
+    "url": "/css/chunk-3c2de56a.261eac10.css"
+  },
+  {
+    "revision": "52f6a6657e493ca0b4c3",
+    "url": "/css/chunk-vendors.10e72359.css"
+  },
+  {
+    "revision": "a53bbb7d9d31254ba12b",
+    "url": "/css/index.af6801d5.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "1eb2e0ed1a5ceb27aa642db37f130e9f",
+    "url": "/img/animal.jpeg"
+  },
+  {
+    "revision": "d5cb2dffa5828108497d7ba300fbfe40",
+    "url": "/img/background.d5cb2dff.png"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "90af01ff5dbba14553ff0535a359a184",
+    "url": "/img/loading.gif"
+  },
+  {
+    "revision": "b05e0dec4be83710a40992310097ac13",
+    "url": "/img/loading.png"
+  },
+  {
+    "revision": "c0cdc79b56d9c69f9307c89cf4ebad4d",
+    "url": "/img/logo.c0cdc79b.png"
+  },
+  {
+    "revision": "581e68f10b15738243db1bde77ac8547",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a0fd4229da1b340bace",
+    "url": "/js/chunk-08c37ec1.c458462b.js"
+  },
+  {
+    "revision": "bf2c307e6401511b9582",
+    "url": "/js/chunk-3c2de56a.ee13b2ad.js"
+  },
+  {
+    "revision": "d06af45e395adf878392",
+    "url": "/js/chunk-e9809e06.ef3fe219.js"
+  },
+  {
+    "revision": "52f6a6657e493ca0b4c3",
+    "url": "/js/chunk-vendors.edd9adbc.js"
+  },
+  {
+    "revision": "a53bbb7d9d31254ba12b",
+    "url": "/js/index.d3e6412a.js"
+  },
+  {
+    "revision": "974d440630ad5d61a5516f5049f5017e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
