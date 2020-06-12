893a6ce50276a68c6dd5a0ecd721794b8ceb6e9e
Mike-Zheng <Mike-Zheng@users.noreply.github.com>
2020-06-12 18:24:56 +0000

    Deploy Commit

---
+++ precache-manifest.0e1df86d658d6de2abe3858e5cb85a11.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d2e05091679ad9e50b6646bc3afc07",
+    "url": "http://nookstamp.com/nookstamp-dev/index.html"
+  },
+  {
+    "revision": "83d83ec8ee956db1a3a7",
+    "url": "http://nookstamp.com/nookstamp-dev/static/css/2.3e659a4a.chunk.css"
+  },
+  {
+    "revision": "387cd0a7468641e5d3a8",
+    "url": "http://nookstamp.com/nookstamp-dev/static/css/main.9fc0e035.chunk.css"
+  },
+  {
+    "revision": "83d83ec8ee956db1a3a7",
+    "url": "http://nookstamp.com/nookstamp-dev/static/js/2.c6a3f3bf.chunk.js"
+  },
+  {
+    "revision": "e82d2d5ae1faae961eedb89cb7304bc0",
+    "url": "http://nookstamp.com/nookstamp-dev/static/js/2.c6a3f3bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "387cd0a7468641e5d3a8",
+    "url": "http://nookstamp.com/nookstamp-dev/static/js/main.b5bb1f02.chunk.js"
+  },
+  {
+    "revision": "e03b02fa22fab564602d",
+    "url": "http://nookstamp.com/nookstamp-dev/static/js/runtime-main.7ed272a3.js"
+  },
+  {
+    "revision": "9c446b5d9dc9d1097dc5e293bc30b478",
+    "url": "http://nookstamp.com/nookstamp-dev/static/media/background.9c446b5d.png"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "http://nookstamp.com/nookstamp-dev/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "http://nookstamp.com/nookstamp-dev/static/media/element-icons.d2f69a92.woff"
+  },
+  {
+    "revision": "5e98861f6ef1ce225121abfb3f418166",
+    "url": "http://nookstamp.com/nookstamp-dev/static/media/getFetch.5e98861f.cjs"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "http://nookstamp.com/nookstamp-dev/precache-manifest.93342893bc02bc80fb86bd2bd676976e.js"
+  "http://nookstamp.com/nookstamp-dev/precache-manifest.0e1df86d658d6de2abe3858e5cb85a11.js"
 );
 
 self.addEventListener('message', (event) => {
